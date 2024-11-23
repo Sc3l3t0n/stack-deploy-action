2cf493be77d74c860e64d170e439b223f6f09463
Marlon Rosenberg <marlon.rosenberg@t-online.de>
2024-11-23 19:39:32 +0100

    Correct comparison

--- src/main.sh
+++ src/main.sh
@@ -60,10 +60,9 @@ if [ -n "${INPUT_ENV_FILE}" ]; then
   # export ENV_FILE="${INPUT_ENV_FILE}"
 fi
 
-if [ -n "${INPUT_PULL}" == "true" ]; then
+if [ "${INPUT_PULL}" == "true" ]; then
   echo -e "\u001b[36mRestoring Stack: \u001b[37;1m${INPUT_NAME}"
   docker compose -f "${INPUT_FILE}" pull
-  sleep 5
 fi
 
 echo -e "\u001b[36mDeploying Stack: \u001b[37;1m${INPUT_NAME}"
